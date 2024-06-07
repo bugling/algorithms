9ec78a35611b7bd4e12fbe472ab8f8012315837c
= <baebuseung@gmail.com>
2024-06-07 16:47:45 +0900

    gitignore 및 자료구조 알고리즘 재반영

--- src/자료구조/카드_게임.java
+++ src/자료구조/카드_게임.java
@@ -6,54 +6,46 @@ import java.util.Scanner;
 
 public class 카드_게임 {
 
-	 
 	/**
+	 * 1~N까지의 카드가 오름차순으로 쌓여있다.
+	 * 가장 위의 카드를 버리고 그 위에 위치한 카드를 가장 아래로 이동한다.
+	 * 마지막에 남는 카드는 무엇일까?
 	 * 
-	 * [ ���� ]
+	 * [ 슈도 코딩 ]
+	 * N: 카드의 개수
+	 * myQueue: 카드 저장 자료구조
 	 * 
-	 * 1~N������ ī�尡 ������������ �׿��ִ�.
-	 * ���� ���� ī��� ������, �� ���� ī��� ���� �Ʒ��� �ű��.
-	 * �� ������ ������ 1�� ���� ������ �ݺ��Ѵ�.
-	 * �������� ���� ī���� ���ڴ� �����ΰ�?
-	 * 
-	 * 
-	 * [ �����ڵ� ]
-	 * 
-	 * N: ī���� ����
-	 * myQueue: ī�� ���� �ڷᱸ��
-	 * 
-	 * for(ī���� ������ŭ �ݺ�)
+	 * for(카드의 개수만큼 반복)
 	 * {
-	 * 		ť�� ī�� �����ϱ�
+	 * 		큐에 카드 저장하기
 	 * }
 	 * 
-	 * while(ī�尡 1�� ���� ������)
+	 * while(카드가 1장 남을 때까지)
 	 * {
-	 * 		�� ���� ī�� ����: poll();
-	 * 		�� ���� ī�� ���� �Ʒ��� ī�� ������ �̵�: poll() -> add();
+	 * 		맨 위의 카드를 버림: poll();
+	 * 		맨 위의 카드를 가장 아래의 카드 밑으로 이동: poll() -> add();
 	 * }
 	 * 
-	 * ���������� ���� ī�� ���
+	 * 마지막에 남은 카드 출력
 	 * 
 	 * **/
 	public static void main(String[] args) {
 		
-		
 		Scanner sc = new Scanner(System.in);
 		Queue<Integer> myQueue = new LinkedList<>();
-		int N = sc.nextInt();
+		int N = sc.nextInt();		// 카드의 개수 저장
 		
+		// 큐에 카드 저장하기
 		for(int i=1; i<=N; i++) {
 			myQueue.add(i);
 		}
 		
 		while(myQueue.size() > 1) {
 			myQueue.poll();
-			int temp = myQueue.poll();
-			myQueue.add(temp);
+			myQueue.add(myQueue.poll());
 		}
 		
-		System.out.println(myQueue.poll());	
+		System.out.println(myQueue.poll());
 	}
 
 }
