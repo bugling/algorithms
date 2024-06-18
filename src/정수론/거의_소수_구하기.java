08a65820dfd8907916585ffebf9d833f99c43894
= <baebuseung@gmail.com>
2024-06-18 11:33:11 +0900

    거의 소수 반영

--- src/정수론/거의_소수_구하기.java
+++ src/정수론/거의_소수_구하기.java
@@ -1,5 +1,7 @@
 package 정수론;
 
+import java.util.Scanner;
+
 /**
  * 백준 1456
  * 
@@ -27,15 +29,65 @@ package 정수론;
  * A: 소수 배열
  * for(2~10,000,000)		// 10^14의 제곱근인 10^7까지 반복
  * {
- * 
+ * 		A배열 초기화하기
  * }
- *      
  * 
+ * for(2~10,000,000)
+ * {
+ * 		A배열에서 소수인 값 일 때
+ * 		temp = 현재 소수
+ * 		
+ * 		// 현재 소수의 제곱근이 Max보다 작을 떄를 기준으로 하지만
+ * 		// 곱셈이 long의 범위를 넘어갈 수 있어 이항 정리로 처리하기
+ * 		while(현재 소수 <= Max/temp)						// A[i] * temp <= Max를 이항정리한 것	(제곱값이 Max를 넘지 않을 때까지 반복한다는 뜻)
+ * 		{
+ * 			if(현재 소수 >= Min/temp) 정답값 증가
+ * 			temp = temp * 현재 소수
+ * 		}
+ * }
+ * 
+ * 정답 출력하기
+ *      
  * **/
 public class 거의_소수_구하기 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		long Min = sc.nextLong();
+		long Max = sc.nextLong();
+		long[] A = new long[10000001];
+		
+		for(int i=2; i<A.length; i++) {
+			A[i] = i;
+		}
+		
+		for(int i=2; i<Math.sqrt(A.length); i++) {		// 제곱근까지만 수행
+			if(A[i] == 0) {
+				continue;
+			}
+			for(int j=2*i; j<A.length; j=j+i) {
+				A[j] = 0;
+			}
+		}
+		
+		int count = 0;
+		for(int i=2; i<10000001; i++) {
+			if(A[i] != 0) {
+				
+				long temp = A[i];			// temp = A[i] ^ 1
+				
+				while((double)A[i] <= (double)Max / (double)temp) {		// A[i] * temp <= Max를 이항정리한 것	(제곱값이 Max를 넘지 않을 때까지 반복한다는 뜻)
+			//		if((double)A[i] >= (double)Min/(double)temp) {
+						count++;
+			//		}
+					
+					temp = temp * A[i];		// A[i]의 제곱값을 temp에 저장한다.
+				}
+			}
+		}
+		
+		System.out.println(count);
 	}
 
 }
